5705b33ee538fc1631806e10bdb036974902f18e
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-06-14 11:07:08 +0300

    serialize_rules

--- server/models.py
+++ server/models.py
@@ -19,6 +19,8 @@ db = SQLAlchemy(metadata=metadata)
 
 class Zookeeper(db.Model, SerializerMixin):
     __tablename__ = 'zookeepers'
+    
+    serialize_rules = ('-animals.zookeper',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
@@ -29,6 +31,8 @@ class Zookeeper(db.Model, SerializerMixin):
 
 class Enclosure(db.Model, SerializerMixin):
     __tablename__ = 'enclosures'
+    
+    serialize_rules = ('-animals.enclosure',)
 
     id = db.Column(db.Integer, primary_key=True)
     environment = db.Column(db.String)
@@ -39,6 +43,8 @@ class Enclosure(db.Model, SerializerMixin):
 
 class Animal(db.Model, SerializerMixin):
     __tablename__ = 'animals'
+    
+    serialize_rules = ('-zookeeper.animals', '-enclosure.animals',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
